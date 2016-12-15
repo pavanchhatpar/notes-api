2c1275bbe045780b9275c009c3a740c92612a9da
pavan_08 <pavanchhatpar@gmail.com>
2016-12-15 13:29:36 +0530

    Initial Commit

---
+++ database/migrations/2016_12_14_202950_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->uuid('uid');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->primary('uid');
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ database/migrations/2016_12_14_212519_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note', function (Blueprint $table) {
+            $table->uuid('nid');
+            $table->uuid('uid');
+            $table->boolean('list');
+            $table->timestamp('reminder');
+            $table->timestamps();
+            $table->primary('nid');
+            $table->foreign('uid')->references('uid')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note');
+    }
+}
