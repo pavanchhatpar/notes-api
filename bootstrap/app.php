bffd35e98ea6077c025f85102e4de11c3de5be22
pavan_08 <pavanchhatpar@gmail.com>
2017-06-05 01:02:49 +0530

    cors support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -65,6 +65,9 @@ $app->singleton(
 // $app->middleware([
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
@@ -84,6 +87,7 @@ $app->singleton(
 
 // $app->register(App\Providers\AppServiceProvider::class);
  $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 //Passport service providers
